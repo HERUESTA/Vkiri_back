55a36ecf5964e4e6e26ba37d2d72953f418345d3
seki <102042736+HERUESTA@users.noreply.github.com>
2025-06-16 22:05:26 +0900

    Lint修正

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-
     config.load_defaults 8.0
     config.autoload_lib(ignore: %w[assets tasks])
     config.api_only = true
